d46031f1e368dff05ac9f32f33454d1af1fa8f9b
a65885898-sudo <a65885898@gmail.com>
2026-08-29 14:30:43 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildVoiceStates
+    ]
+});
+
+client.once("ready", () => {
+    console.log(`🔵 ${client.user.tag} is online!`);
+});
+
+client.login(process.env.TOKEN);
